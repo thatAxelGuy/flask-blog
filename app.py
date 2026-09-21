7975b6816347227f891bdf244224b5f3bb800067
thataxelguy <axel.woermann@gmail.com>
2026-09-21 16:35:13 +0200

    Add route for creating blog posts

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 
 BLOG_POSTS = "blog_posts.json"
 
@@ -8,7 +8,6 @@ def load_posts():
     try:
         with open(BLOG_POSTS, "r") as file:
             blog_posts = json.load(file)
-            print("hello")
     except FileNotFoundError:
         blog_posts = []
 
@@ -17,7 +16,11 @@ def load_posts():
 
     return blog_posts
 
-    
+
+def save_posts(blog_posts):
+    with open(BLOG_POSTS, "w") as file:
+        json.dump(blog_posts, file, indent=4)
+
 
 app = Flask(__name__)
 
@@ -26,6 +29,32 @@ app = Flask(__name__)
 def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        author = request.form['author']
+        title = request.form['title']
+        content = request.form['content']
+
+        blog_posts = load_posts()
+
+        new_id = max([post['id'] for post in blog_posts], default=0) + 1
+
+        new_post = {
+            "id": new_id,
+            "author": author,
+            "title": title,
+            "content": content
+        }
+
+        blog_posts.append(new_post)
+
+        save_posts(blog_posts)
+
+        return redirect('/')
+    # If the request is GET, show the add post form
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
